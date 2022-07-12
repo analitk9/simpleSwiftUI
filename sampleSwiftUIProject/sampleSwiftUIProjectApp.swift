04d15a3c92a4f45a6b078fb0fcffd31ac3f20661
Evdokimov Denis <analitik9@gmail.com>
2022-07-12 14:44:08 +0300

    Initial Commit

---
+++ sampleSwiftUIProject/sampleSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleSwiftUIProjectApp.swift
+//  sampleSwiftUIProject
+//
+//  Created by Denis Evdokimov on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct sampleSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
