21403e258037466e48e3f3c9f1d7dc01e0afbf01
Evdokimov Denis <analitik9@gmail.com>
2022-07-17 19:15:37 +0300

    regular modifier

---
+++ sampleSwiftUIProject/Demo/RegularModifier.swift
@@ -0,0 +1,20 @@
+//
+//  RegularModifier.swift
+//  sampleSwiftUIProject
+//
+//  Created by Denis Evdokimov on 7/17/22.
+//
+
+import SwiftUI
+
+struct RegularModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegularModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        RegularModifier()
+    }
+}
